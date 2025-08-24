499a36a3ff45c9f78c0cbdf936ad299b8f2ea3c5
Amirtx7x <amirtx7@gmail.com>
2025-08-24 19:35:11 +0300

    Add .gitignore file

--- .history/client/src/components/Footer_20250601175056.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer style={{ textAlign: 'center', padding: '1rem', backgroundColor: '#eee', marginTop: '2rem' }}>
-            <p>© 2025 147 Fitness Store. All Rights Reserved.</p>
-        </footer>
-    );
-}
-
-export default Footer;
